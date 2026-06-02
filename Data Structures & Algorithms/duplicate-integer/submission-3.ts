5b0c961dea84d96f61237d90b8a6b9f2cee309a3
Julio Hernandez, Jr. <132781123+useJediMindTrick@users.noreply.github.com>
2026-06-01 21:46:12 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+
+        const seenNums = new Set();
+
+        for (let i = 0; i <nums.length; i++){
+            if (seenNums.has(nums[i])){
+                return true;
+            } 
+            seenNums.add(nums[i]);
+        }
+
+        return false;
+        
+    }
+}
