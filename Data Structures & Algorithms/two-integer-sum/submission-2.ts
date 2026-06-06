337c28a8c1ec446e3277cd3b23d1ab3bf18db557
Julio Hernandez, Jr. <132781123+useJediMindTrick@users.noreply.github.com>
2026-06-06 17:15:32 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        
+        let prevMap:Map<number,number> = new Map();
+
+        for (let idx=0; idx < nums.length; idx++){
+            
+            let diff = target - nums[idx];
+
+            if (prevMap.has(diff)){
+                return [prevMap.get(diff), idx]
+            }
+
+            prevMap.set(nums[idx], idx);
+        }
+    }
+}
